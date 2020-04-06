6c4e5626374e487359b8fadc834d7594d130b004
poem <normamoras@gmail.com>
2020-04-05 23:31:47 -0400

    Updates

---
+++ precache-manifest.64e8f5d712c1e61b87a4d2474fccd397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7797cfe9c1b9568c24c6c0f559e411f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d726ec7707b1e716c8a9",
+    "url": "/static/css/main.63cde0f3.chunk.css"
+  },
+  {
+    "revision": "a02df80889b0bbd94cb1",
+    "url": "/static/js/2.6f5751fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f5751fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d726ec7707b1e716c8a9",
+    "url": "/static/js/main.effb42bf.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
